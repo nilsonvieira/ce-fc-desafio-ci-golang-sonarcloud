41ae5cd069e891ffc18bd6cc60c8e926140ef342
Nilson R S Vieira <nilsonrsvieira@gmail.com>
2022-07-07 22:44:48 -0300

    ci: Creating Covered tests

--- sum_test.go
+++ sum_test.go
@@ -9,3 +9,27 @@ func TestSum(t *testing.T) {
 		t.Error("The result must be 5")
 	}
 }
+
+func TestSub(t *testing.T) {
+	result := sub(3, 1)
+
+	if result != 2 {
+		t.Error("The result must be 2")
+	}
+}
+
+func TestTimes(t *testing.T) {
+	result := times(3, 2)
+
+	if result != 6 {
+		t.Error("The result must be 6")
+	}
+}
+
+func TestSumX(t *testing.T) {
+	result := sumX(3, 2)
+
+	if result != 8 {
+		t.Error("The result must be 8")
+	}
+}
